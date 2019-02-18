28f32975f07e70ad4479b88ce2e1e100c157594b
Mateus Araújo <mateuscruz22@gmail.com>
2019-02-18 19:30:35 -0300

    Adicionando procedimento inserirNoArquivo

--- arquivo.c
+++ arquivo.c
@@ -6,6 +6,14 @@ typedef struct tipoAluno{
     int matricula, dia, mes, ano;
 } aluno;
 
+void inserirNoArquivo(FILE *pArquivo, aluno *aux){
+    if(pArquivo != NULL){
+        fprintf(pArquivo, "%s %d %d/%d/%d\n", aux->nome, aux->matricula, aux->dia, aux->mes, aux->ano);
+    }else{
+        printf("Erro ao ler o arquivo!\n");
+    }
+}
+
 int main(int argc, char *argv[]){
 
     aluno a;
