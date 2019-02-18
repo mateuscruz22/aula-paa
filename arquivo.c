9591b175f1acc764f5c160e4fc0c69156916673e
Mateus Araújo <mateuscruz22@gmail.com>
2019-02-18 19:23:44 -0300

    Definindo a struct aluno

---
+++ arquivo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct tipoAluno{
+    char nome[255];
+    int matricula, dia, mes, ano;
+} aluno;
+
+int main(int argc, char *argv[]){
+
+    aluno a;
+
+    return 0;
+}
